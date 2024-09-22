4626c12a16b9f9ac9ae75c57d31bac2adc851619
making-a-scene <4936joo@naver.com>
2024-09-22 20:05:33 +0900

    ps: 프로그래머스 순위; 플로이드-워셜 알고리즘 사용을 통한 최적화

--- 김승주/programmers/Graph/순위/Solution.java
+++ 김승주/programmers/Graph/순위/Solution.java
@@ -1,48 +1,39 @@
-import java.util.*;
-
 class Solution {
-    static Set<Integer> candidates = new HashSet<>();
-    static List<Integer>[] graph;
-
     public int solution(int n, int[][] results) {
-        int[] degree = new int[n + 1];
-        graph = new List[n + 1];
-        for (int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();
-            candidates.add(i);
+        int answer = 0;
+        int[][] floyd = new int[n+1][n+1];
+        
+        for(int i = 0; i < results.length; i++){
+            int A = results[i][0];
+            int B = results[i][1];
+            
+            floyd[A][B] = 1; 
+            floyd[B][A] = -1; 
         }
-        for (int[] result : results) {
-            graph[result[0]].add(result[1]);
-            degree[result[1]]++;
-        }
-        topologicalSort(degree, new boolean[n + 1]);
-        return candidates.size();
-    }
-
-    private static void topologicalSort(int[] degree, boolean[] visited) {
-        List<Integer> start = new ArrayList<>();
-        for (int i = 1; i < degree.length; i++) {
-            if (degree[i] == 0 && !visited[i]) {
-                start.add(i);
-            }
-        }
-        if (start.size() != 1) {
-            candidates.removeAll(start);
-            for (int num : start) {
-                int[] nextDegree = Arrays.copyOf(degree, degree.length);
-                boolean[] nextVisited = Arrays.copyOf(visited, visited.length);
-                nextVisited[num] = true;
-                for (int next : graph[num]) {
-                    nextDegree[next]--;
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                for(int k = 1; k <= n; k++){
+                    if(floyd[i][k] == 1 && floyd[k][j] == 1){
+                        floyd[i][j] = 1;
+                        floyd[j][i] = -1;
+                    }
+                    if(floyd[i][k] == -1 && floyd[k][j] == -1){
+                        floyd[i][j] = -1;
+                        floyd[j][i] = 1;
+                    }
                 }
-                topologicalSort(nextDegree, nextVisited);
             }
-        } else {
-            visited[start.get(0)] = true;
-            for (int next : graph[start.get(0)]) {
-                degree[next]--;
-            }
-            topologicalSort(degree, visited);
         }
+        
+        for(int i = 1; i <= n; i++){
+            int cnt = 0; 
+            for(int j = 1; j <= n; j++){
+                if(floyd[i][j] != 0) cnt++;
+            }
+            if(cnt == n-1) answer++;
+        }
+        
+        return answer;
     }
 }
