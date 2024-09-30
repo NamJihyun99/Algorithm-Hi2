c6b323414264060d98ce193513682912aafe2bc0
making-a-scene <4936joo@naver.com>
2024-09-30 14:36:02 +0900

    refactor: 백준 2342 Dance Dance Revolution 코드 최적화

--- 김승주/baekjoon/DynamicProgramming/2342 Dance Dance Revolution/Main.java
+++ 김승주/baekjoon/DynamicProgramming/2342 Dance Dance Revolution/Main.java
@@ -4,7 +4,6 @@ import java.util.*;
 class Main {
     static int[] input = new int[100002];
     static int[][][] dp = new int[5][5][100002];
-    static int min = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -14,97 +13,26 @@ class Main {
                 break;
             }
         }
-        ddr(0, 0, 1);
-        System.out.println(min);
+        System.out.println(ddr(0, 0, 1));
     }
-    private static void ddr(int left, int right, int cnt) {
+    private static int ddr(int left, int right, int cnt) {
         if (input[cnt] == 0) {
-            min = Math.min(min, dp[left][right][cnt - 1]);
-            return;
+            return 0;
         }
 
-        if (dp[left][right][cnt - 1] >= min) {
-            return;
+        if (dp[left][right][cnt] == 0) {
+            dp[left][right][cnt] = Math.min(energy(left, input[cnt]) + ddr(input[cnt], right, cnt + 1), 
+            energy(right, input[cnt]) + ddr(left, input[cnt], cnt + 1));
         }
 
-        if (input[cnt] == 1) {
-            if (left == 0) {
-                dp[input[cnt]][right][cnt] = dp[left][right][cnt - 1] + 2;
-            } else if (left == 1) {
-                dp[input[cnt]][right][cnt] = dp[left][right][cnt - 1] + 1;
-            } else if (left == 2 || left == 4) {
-                dp[input[cnt]][right][cnt] = dp[left][right][cnt - 1] + 3;
-            } else {
-                dp[input[cnt]][right][cnt] = dp[left][right][cnt - 1] + 4;
-            }
-            if (right == 0) {
-                dp[left][input[cnt]][cnt] = dp[left][right][cnt - 1] + 2;
-            } else if (right == 1) {
-                dp[left][input[cnt]][cnt] = dp[left][right][cnt - 1] + 1;
-            } else if (right == 2 || right == 4) {
-                dp[left][input[cnt]][cnt] = dp[left][right][cnt - 1] + 3;
-            } else {
-                dp[left][input[cnt]][cnt] = dp[left][right][cnt - 1] + 4;
-            }
-        } else if (input[cnt] == 2) {
-            if (left == 0) {
-                dp[input[cnt]][right][cnt] = dp[left][right][cnt - 1] + 2;
-            } else if (left == 2) {
-                dp[input[cnt]][right][cnt] = dp[left][right][cnt - 1] + 1;
-            } else if (left == 1 || left == 3) {
-                dp[input[cnt]][right][cnt] = dp[left][right][cnt - 1] + 3;
-            } else {
-                dp[input[cnt]][right][cnt] = dp[left][right][cnt - 1] + 4;
-            }
-            if (right == 0) {
-                dp[left][input[cnt]][cnt] = dp[left][right][cnt - 1] + 2;
-            } else if (right == 2) {
-                dp[left][input[cnt]][cnt] = dp[left][right][cnt - 1] + 1;
-            } else if (right == 1 || right == 3) {
-                dp[left][input[cnt]][cnt] = dp[left][right][cnt - 1] + 3;
-            } else {
-                dp[left][input[cnt]][cnt] = dp[left][right][cnt - 1] + 4;
-            }
-        } else if (input[cnt] == 3) {
-            if (left == 0) {
-                dp[input[cnt]][right][cnt] = dp[left][right][cnt - 1] + 2;
-            } else if (left == 3) {
-                dp[input[cnt]][right][cnt] = dp[left][right][cnt - 1] + 1;
-            } else if (left == 2 || left == 4) {
-                dp[input[cnt]][right][cnt] = dp[left][right][cnt - 1] + 3;
-            } else {
-                dp[input[cnt]][right][cnt] = dp[left][right][cnt - 1] + 4;
-            }
-            if (right == 0) {
-                dp[left][input[cnt]][cnt] = dp[left][right][cnt - 1] + 2;
-            } else if (right == 3) {
-                dp[left][input[cnt]][cnt] = dp[left][right][cnt - 1] + 1;
-            } else if (right == 2 || right == 4) {
-                dp[left][input[cnt]][cnt] = dp[left][right][cnt - 1] + 3;
-            } else {
-                dp[left][input[cnt]][cnt] = dp[left][right][cnt - 1] + 4;
-            }
-        } else {
-            if (left == 0) {
-                dp[input[cnt]][right][cnt] = dp[left][right][cnt - 1] + 2;
-            } else if (left == 4) {
-                dp[input[cnt]][right][cnt] = dp[left][right][cnt - 1] + 1;
-            } else if (left == 1 || left == 3) {
-                dp[input[cnt]][right][cnt] = dp[left][right][cnt - 1] + 3;
-            } else {
-                dp[input[cnt]][right][cnt] = dp[left][right][cnt - 1] + 4;
-            }
-            if (right == 0) {
-                dp[left][input[cnt]][cnt] = dp[left][right][cnt - 1] + 2;
-            } else if (right == 4) {
-                dp[left][input[cnt]][cnt] = dp[left][right][cnt - 1] + 1;
-            } else if (right == 1 || right == 3) {
-                dp[left][input[cnt]][cnt] = dp[left][right][cnt - 1] + 3;
-            } else {
-                dp[left][input[cnt]][cnt] = dp[left][right][cnt - 1] + 4;
-            }
-        }        
-        ddr(left, input[cnt], cnt + 1);
-        ddr(input[cnt], right, cnt + 1);
+        return dp[left][right][cnt];
     }
+
+    private static int energy(int pos, int des) {
+		int num = Math.abs(pos - des);
+		if (pos == 0) return 2;
+		else if (num == 0) return 1;
+		else if (num == 1 || num == 3) return 3;
+		else return 4;
+	}
 }
