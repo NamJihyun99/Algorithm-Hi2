0ca2737c9c052ad9416d9f9e9bfca400b208e19a
NamJihyun99 <87467801+NamJihyun99@users.noreply.github.com>
2024-11-22 17:20:35 +0900

    ps: 프로그래머스 - 로또의 최고 순위와 최저 순위

---
+++ 남지현/simulation/LottoPossibleRanks.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+// 프로그래머스 - 로또의 최고 순위와 최저 순위
+
+class Solution {   
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] rank = {6, 6, 5, 4, 3, 2, 1};
+        long lotto = 0, win = 0;
+        int unknown = 0;
+        for (int number: lottos) {
+            if (number == 0) unknown++;
+            else lotto |= 1L<<number;
+        }
+        for (int number: win_nums) {
+            win |= 1L<<number;
+        }
+        int matched = numberOfOne(lotto&win);
+        int max = Math.min(unknown, 6-matched);
+        return new int[]{rank[matched+max], rank[matched]};
+    }
+    
+    private int numberOfOne(long bits) {
+        String binaryString = Long.toString(bits, 2);
+        int count = 0;
+        for (int i=0; i<binaryString.length(); i++) {
+            if (binaryString.charAt(i) == '1') count++;
+        }
+        return count;
+    }
+}
