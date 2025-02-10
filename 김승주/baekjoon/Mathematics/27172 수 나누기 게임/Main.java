c22df19695fc9afb3c786d673c9ea9ec67788797
making-a-scene <4936joo@naver.com>
2025-02-10 19:55:51 +0900

    refactor: 백준 27172 수 나누기 게임

--- 김승주/baekjoon/Mathematics/27172 수 나누기 게임/Main.java
+++ 김승주/baekjoon/Mathematics/27172 수 나누기 게임/Main.java
@@ -1,30 +1,38 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        Map<Integer, Integer> cards = new HashMap<>();
-        int[] scores = new int[N];
+        int[] cards = new int[N];
+        boolean[] exists = new boolean[1000001];
+        int[] score = new int[1000001];
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            cards.put(Integer.valueOf(st.nextToken()), i);
+            cards[i] = Integer.parseInt(st.nextToken());
+            // 입력에 존재하는 숫자에 대해 exists 배열에 true로 마킹
+            exists[cards[i]] = true;
         }
-        List<Integer> sorted = new ArrayList<>(cards.keySet());
-        Collections.sort(sorted);
-        for (int i = 0; i < N; i++) {
-            for (int j = N - 1; (sorted.get(i) << 1) <= sorted.get(j); j--) {
-                if (sorted.get(j) % sorted.get(i) == 0) {
-                    scores[cards.get(sorted.get(i))]++;
-                    scores[cards.get(sorted.get(j))]--;
+		
+        // 모든 카드를 순회하며 반복
+        for (int i : cards) {
+        	// 현재 카드의 모든 배수들을 확인
+            for (int j = i * 2; j < 1000001; j += i) {
+            	// i의 배수인 j가 입력에 존재했다면 i의 승점 증가, j의 승점 감소
+                if (exists[j]) {
+                    score[i]++;
+                    score[j]--;
                 }
             }
         }
+        
         StringBuilder sb = new StringBuilder();
-        for (int score : scores) {
-            sb.append(score).append(" ");
+        for (int num : cards) {
+            sb.append(score[num]).append(" ");
         }
+        
         System.out.println(sb.toString());
     }
 }
