9374122010ffc707728bc50769e304a4982aba27
NamJihyun99 <87467801+NamJihyun99@users.noreply.github.com>
2025-02-22 17:57:00 +0900

    ps: 백준 11501번 주식

---
+++ 남지현/stack/Stock11501.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+// 백준 11501번 주식
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int t=0; t<T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            ArrayDeque<Integer> stack = new ArrayDeque<>();
+            long answer = 0l;
+            for (int i=0; i<N; i++) {
+                stack.addLast(Integer.valueOf(st.nextToken()));
+            }
+            while (!stack.isEmpty()) {
+                Integer top = stack.pollLast();
+                Long count = 0l;
+                while (!stack.isEmpty() && stack.peekLast() <= top) {
+                    answer -= stack.pollLast();
+                    count++;
+                }
+                answer += count*top;
+            }
+            sb.append(answer).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
