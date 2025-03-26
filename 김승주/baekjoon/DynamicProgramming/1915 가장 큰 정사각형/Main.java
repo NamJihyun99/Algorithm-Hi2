e0f487f2fb3b3fe872409ef95b3126d25e3a47c3
making-a-scene <4936joo@naver.com>
2025-03-26 13:28:29 +0900

    refactor: 백준 1915 가장 큰 정사각형 시간복잡도 최적화

--- 김승주/baekjoon/DynamicProgramming/1915 가장 큰 정사각형/Main.java
+++ 김승주/baekjoon/DynamicProgramming/1915 가장 큰 정사각형/Main.java
@@ -2,82 +2,45 @@ import java.io.*;
 import java.util.*;
 
 class Main {
+    static int n;
+    static int m;
+    static char[][] arr;
+    static int[][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        char[][] arr = new char[n][m];
-        int[][] dp = new int[n][m];
-        List<int[]> oneLoc = new ArrayList<>();
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new char[n][m];
+        dp = new int[n][m]; // (n, m)을 오른쪽 아래 점으로 하여 만들 수 있는 정사각형의 최대 크기
+        int answer = 0;
         
         for (int i = 0; i < n; i++) {
             String input = br.readLine();
             for (int j = 0; j < m; j++) {
                 arr[i][j] = input.charAt(j);
                 if (arr[i][j] == '1') {
-                    dp[i][j] = 1;
-                    oneLoc.add(new int[] {i, j});
-                }
-            }
-        }
-
-        if (oneLoc.isEmpty()) {
-            System.out.println(0);
-            return;
-        }
-
-        if (oneLoc.size() == n * m) {
-            if (n > m) {
-                System.out.println(m * m);
-            } else {
-                System.out.println(n * n);
-            }
-            return;
-        }
-
-        int answer = 1;
-
-        for (int[] loc : oneLoc) {
-            int length = dp[loc[0]][loc[1]] + 1;
-            while (true) {
-                if (loc[0] + length - 1 >= n || loc[1] + length - 1 >= m) {
-                    break;
-                }
-                boolean flag = false;
-                
-                for (int j = loc[1], width = 1; width < length; j++, width++) {
-                    if (arr[loc[0] + length - 1][j] == '0') {
-                        flag = true;
-                        break;
-                    }
-                }
-                if (flag) {
-                    break;
-                }
-
-                for (int j = loc[0], width = 1; width < length; j++, width++) {
-                    if (arr[j][loc[1] + length - 1] == '0') {
-                        flag = true;
-                        break;
-                    }
-                }
-                if (flag) {
-                    break;
-                }
-
-                length++;
-            }
-
-            answer = Math.max(answer, --length);
-
-            for (int i = loc[0]; length - i > 0; i++) {
-                for (int j = loc[1]; length - j > 0; j++) {
-                    dp[i][j] = Math.min(loc[0] + length - i, loc[1] + length - j);
+                    dp[i][j] = -1;
+                    makeMaxSquare(i, j);
+                    answer = Math.max(answer, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
                 }
             }
         }
 
         System.out.println(answer * answer);
+    }
+
+    private static int makeMaxSquare(int row, int col) {
+        if (row < 0 || row >= n || col < 0 || col >= m) {
+            return 0;
+        }
+
+        if (dp[row][col] == -1) {
+            dp[row][col] = (row == 0 || col == 0)? 1 : Math.min(makeMaxSquare(row - 1, col), Math.min(makeMaxSquare(row, col - 1), makeMaxSquare(row - 1, col - 1))) + 1;
+        }
+
+        return dp[row][col];
     }
 }
