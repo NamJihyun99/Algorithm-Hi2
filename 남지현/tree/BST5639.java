1b2af9d85024f1a0cb2e828ae45bc4bd560a9fd0
NamJihyun99 <87467801+NamJihyun99@users.noreply.github.com>
2025-04-27 22:47:37 +0900

    ps: 백준 5639번 이진 검색 트리

---
+++ 남지현/tree/BST5639.java
@@ -0,0 +1,53 @@
+import java.io.*;
+
+// 백준 5639번 이진 검색 트리
+class Main {
+    static StringBuilder sb;
+
+    static class Node {
+        int number;
+        Node left;
+        Node right;
+
+        Node (int number) {
+            this.number = number;
+        }
+
+        void makeTree(int num) {
+            if (this.number > num ){
+                if (this.left == null) {
+                    this.left = new Node(num);
+                } else {
+                    this.left.makeTree(num);
+                }
+            } else {
+                if (this.right == null) {
+                    this.right = new Node(num);
+                } else {
+                    this.right.makeTree(num);
+                }
+            }
+        }
+    }
+
+    static void postPrint(Node node) {
+        if (node == null) {
+            return;
+        }
+        postPrint(node.left);
+        postPrint(node.right);
+        sb.append(node.number).append("\n");
+    }
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        Node root = new Node(Integer.parseInt(line));
+        while ((line = br.readLine()) != null) {
+            root.makeTree(Integer.parseInt(line));
+        }
+        sb = new StringBuilder();
+        postPrint(root);
+        System.out.print(sb);
+    }
+}
